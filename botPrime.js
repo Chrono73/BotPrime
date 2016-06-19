c8ad11f541422f7d603772814543ec28ce102307
Martin Locqueville <Martin@pc8.home>
2016-06-20 00:57:00 +0200

    Ajout de la commande sortie, ainsi que des items vendus par Baro.

--- botPrime.js
+++ botPrime.js
@@ -1,6 +1,6 @@
 /* 
  * Lien à mettre dans le navigateur pour intégrer le bot à un chan:
- * https://discordapp.com/oauth2/authorize?client_id=188203858860703744&scope=bot&permissions=66186303
+ * https://discordapp.com/oauth2/authorize?client_id=194202468173742080&scope=bot&permissions=66186303
  * 
  * Laisser les permissions suivantes, décocher les autres:
  *      - Se connecter                          - Lire, gérer & envoyer des messages
@@ -31,6 +31,43 @@ var kenOneLiners = [
     "Eh bien si lui fait des miracles, tu peux m'appeler Jean-Baptiste.",
     "Tu es Ken le survivant et tu as suivi l'enseignement de l'école du Hokuto, ou tard?\n- Plutôt tard...\n- Plutôt tard que jamais, là est le principal.",
     "Attaque Nanto, le poing du guerrier dans la figure !\n- Attaque Hokuto, coup de pied volant avec pointure !"
+];
+
+var sortieBosses = [
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "du Raptor"
+];
+
+var sortieMissions = [
+    "?",
+    "Survie",
+    "?",
+    "Sabotage",
+    "?",
+    "?",
+    "?",
+    "?",
+    "?",
+    "Assassinat"
+];
+
+var sortieMods = [
+    "?",
+    "?",
+    "Amélioration physique des ennemis",
+    "Réduction d'Energie",
+    "?",
+    "Pistolets uniquement"
 ];
 
 var rufFlightAcademyLessons = 0;
@@ -94,6 +131,8 @@ function computeTime(time) {
     return [j, h, m, s];
 }
 
+
+
 /*botPrime.on("ready"), function(){
     var connectMsg = "Bonjour, le bot a été update ! Venez voir les changements sur le GitHub!";
     botPrime.servers.forEach(function(serv){
@@ -109,14 +148,17 @@ botPrime.on("message", function(message) {
             helpMsg += "\t- !help:    affiche ce message.\n";
             helpMsg += "\t- !git:     affiche le lien du projet GitHub.\n";
             helpMsg += "\t- !alerts:  affiche les alertes Warframe du moment.\n";
+            helpMsg += "\t- !sortie:  affiche les trois missions de la sortie d'aujourd'hui.\n";
             helpMsg += "\t- !baro:    affiche les informations liées au Void Trader.\n\n";
             helpMsg += "Tout ça avec quelques easter eggs. Et ne me fais pas répéter !\n";
             botPrime.reply(message, helpMsg);
             break;
+        //========================
         
         case "!git":
             botPrime.reply(message, "https://github.com/Chrono73/BotPrime");
             break;
+        //========================
             
         case "!alerts":
             request({
@@ -142,7 +184,7 @@ botPrime.on("message", function(message) {
                                 result += reward.substring(reward.lastIndexOf('/')+1);
                             })
                         };
-                        rmtime = rmtime = computeTime(alert.Expiry.sec - body.Time);
+                        rmtime = computeTime(alert.Expiry.sec - body.Time);
                         result += " - " + rmtime[2] + "m, " + rmtime[3] + "s restantes";
                         result += "\n";
                     });
@@ -156,6 +198,27 @@ botPrime.on("message", function(message) {
                 };
             })
             break;
+        //========================
+
+        case "!sortie":
+            request({
+                url: wfdata,
+                json: true
+            }, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    var result = "Battez les troupes " + sortieBosses[body.Sorties[0].Variants[0].bossIndex] + "!\n";
+                    var missionCount = 1;
+                    body.Sorties[0].Variants.forEach( function(sortie){
+                        result += "Mission "+missionCount+": "+sortieMissions[sortie.missionIndex]+" / "+sortieMods[sortie.modifierIndex]+"\n";
+                        missionCount++;
+                    })
+                    rmtime = computeTime(body.Sorties[0].Expiry.sec - body.Time);
+                    result += "Temps restant: " + rmtime[1] + "h, " + rmtime[2] + "m, " + rmtime[3] + "s restantes";
+                    botPrime.reply(message, result);
+                }
+            });
+            break;
+        //========================
             
         case "!baro":
             request({
@@ -165,34 +228,51 @@ botPrime.on("message", function(message) {
                 if (!error && response.statusCode === 200) {
                     var result = "\n";
                     result += "Baro'Ki Teer - Void Trader\n";
-                    rmtime = computeTime(body.VoidTraders[0].Activation.sec - body.Time);
-                    result += "Arrive dans " + rmtime[0] + " jours, " + rmtime[1] + " heures, " + rmtime[2] + " minutes et " + rmtime[3] + " secondes.\n";
-                    result += "Il apparaîtra au relais de ";
-                    if (body.VoidTraders[0].Node == "MercuryHUB") result += "Mercure.\n";
-                    else if (body.VoidTraders[0].Node == "SaturnHUB") result += "Saturne.\n";
-                    else result += "Pluton.\n";
-                    if (result != "")
-                        botPrime.reply(message, result);
-                    else
-                        botPrime.reply(message, "Erreur lors de la récupération des données.");
+                    if (body.VoidTraders[0].hasOwnProperty("Manifest")) {
+                        rmtime = computeTime(body.VoidTraders[0].Expiry.sec - body.Time);
+                        result += "Part dans " + rmtime[0] + " jour, " + rmtime[1] + " heures, " + rmtime[2] + " minutes et " + rmtime[3] + " secondes.\n";
+                        body.VoidTraders[0].Manifest.forEach( function(item){
+                            result += item.ItemType.substring(reward.lastIndexOf('/')+1); + " - " + item.PrimePrice + " ducats & " + item.RegularPrice + "cr\n";
+                        });
+                        if (result != "")
+                            botPrime.reply(message, result);
+                        else
+                            botPrime.reply(message, "Erreur lors de la récupération des données.");
+                    }
+                    else {
+                        rmtime = computeTime(body.VoidTraders[0].Activation.sec - body.Time);
+                        result += "Arrive dans " + rmtime[0] + " jours, " + rmtime[1] + " heures, " + rmtime[2] + " minutes et " + rmtime[3] + " secondes.\n";
+                        result += "Il apparaîtra au relais de ";
+                        if (body.VoidTraders[0].Node == "MercuryHUB") result += "Mercure.\n";
+                        else if (body.VoidTraders[0].Node == "SaturnHUB") result += "Saturne.\n";
+                        else result += "Pluton.\n";
+                        if (result != "")
+                            botPrime.reply(message, result);
+                        else
+                            botPrime.reply(message, "Erreur lors de la récupération des données.");
+                    }
                 }
             });
             break;
+        //========================
 
         // Easter Eggs
         case "!ken":
             res = Math.floor((Math.random() * kenOneLiners.length));
             botPrime.reply(message, kenOneLiners[res]);
             break;
+        //========================
 
         case "Paladin, niveau 66.":
             botPrime.reply(message, "Archmâââge, 57.\n");
             break;
+        //========================
 
         case "On fouille les cadavres pour le butin.":
-        cible = message.server.members[Math.floor((Math.random() * message.server.members.length))].username;
+            cible = message.server.members[Math.floor((Math.random() * message.server.members.length))].username;
             botPrime.reply(message, "Trop tard, "+cible+" s'en est déjà chargé pendant que vous vous battiez.\n");
             break;
+        //========================
             
         case "!bluekutku":
             var prime = 0;
@@ -206,6 +286,7 @@ botPrime.on("message", function(message) {
                 botPrime.reply(message, "\nKseniva carted.\nQuest reward reduced to "+prime+".\nReturning to camp.");
             }
             break;
+        //========================
 
         default:
             break;
@@ -218,8 +299,21 @@ botPrime.on("serverNewMember", (server, user) => {
     //botPrime.sendMessage(server.defaultChannel, welcomeMsg);
 })
 
+/*
+fs.readFile('./token', 'utf8', function read(err, token){
+    if (err) {
+        throw err;
+    }
+    else {
+        botPrime.loginWithToken(token);
+        console.log("Bot en ligne et prêt!\n");
+    }
+});
+*/
+
 botPrime.loginWithToken(process.env.TOKEN);
 console.log("Bot en ligne et prêt!\n");
+
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
